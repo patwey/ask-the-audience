2478f8d7265cda4eb3ebb4922309e1144946136e
Pat Wey <patrickwey@gmail.com>
2016-03-02 13:30:32 -0700

    Sending messages to a particular client

--- public/client.js
+++ public/client.js
@@ -6,3 +6,9 @@ var connectionCount = document.getElementById('connection-count');
 socket.on('usersConnected', function (count) {
   connectionCount.innerText = 'Connected Users: ' + count;
 });
+
+var statusMessage = document.getElementById('status-message');
+
+socket.on('statusMessage', function (msg) {
+  statusMessage.innerText = msg;
+});
